08f065908de281a781f5935e461c7b2c498145b5
pedrohenriquecrispim <teixerahac@gmail.com>
2023-09-28 12:07:55 -0300

    codigo atualizado

--- arquivo.py
+++ arquivo.py
@@ -1,16 +1,38 @@
 # importa o módulo datetime no início do seu código
 from datetime import datetime
 import time  # importa o módulo time para a contagem regressiva
+import json
+
+# Carrega os dados do arquivo JSON no início do programa
+def carregar_dados():
+    try:
+        with open('dados.json', 'r') as arquivo:
+            return json.load(arquivo)
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        print("Erro ao carregar os dados do arquivo JSON.")
+        return []
+    
+# Salva os dados em um arquivo JSON
+def salvar_dados(visitantes):
+    try:
+        with open('dados.json', 'w') as arquivo:
+            json.dump(visitantes, arquivo, indent=4)
+        print("Dados salvos com sucesso.")
+    except Exception as e:
+        print(f"Erro ao salvar os dados: {e}")
+
 
 # dados iniciais
-visitantes = []
+visitantes = carregar_dados()
 
-# função para exibir o menu
+# Função para exibir o menu
 def exibir_menu():
     print("Selecione uma opção:")
     print("1 - Registrar novo visitante")
     print("2 - Pesquisar visitante")
-    print("0 - Encerrar programa")
+    print("0 - Salvar e encerrar")
 
 # função para realizar a contagem regressiva de emergência
 def contagem_regressiva():
@@ -29,12 +51,12 @@ def contagem_regressiva():
     if ligar_para_policia == "S":
         endereco = input("Digite o endereço do local com CEP: ")
         ocorrido = input("Descreva o ocorrido: ")
-
+        
         # mostrar informações coletadas
         print("\nInformações da emergência:")
         print(f"Endereço com CEP: {endereco}")
         print(f"Descrição do ocorrido: {ocorrido}")
-
+        
         # confirmar informações
         confirmacao = input("Confirma as informações? (S/N)").upper()
         if confirmacao == "S":
@@ -109,7 +131,7 @@ def registrar_visitante():
             else:
                 print("Resposta inválida. Por favor, digite 'S' para Sim ou 'N' para Não.")
 
-# função para pesquisar visitantes com o mesmo nome
+# Função para pesquisar visitantes com o mesmo nome
 def pesquisar_visitante():
     nome_pesquisa = input("Digite o nome do visitante que deseja pesquisar: ")
     encontrados = []
@@ -126,11 +148,12 @@ def pesquisar_visitante():
             print(f"Motivo da visita: {visitante['Motivo']}")
             print(f"Horário de entrada: {visitante['Entrada']}")
             print(f"Horário de saída: {visitante['Saída']}")
-            print("-" * 20) 
+            print("-" * 20)
     else:
         print("Visitante não encontrado.")
 
-# função principal do menu
+
+# Função principal do menu
 def main():
     while True:
         exibir_menu()
@@ -139,15 +162,23 @@ def main():
             registrar_visitante()
         elif opcao == "2":
             pesquisar_visitante()
+        elif opcao == "3":
+            salvar_dados(visitantes)
+            print("Encerrando o programa.")
+            break
         elif opcao == "0":
+            salvar_dados(visitantes)
             break
         else:
             print("Opção inválida. Tente novamente.")
 
-# iniciar o programa
+# Iniciar o programa
 if __name__ == "__main__":
-    tem_emergencia = input("Tem alguma emergência? (S/N)").upper()
-    if tem_emergencia == "S":
-        contagem_regressiva()
-    else:
-        main()  # executa o menu de cadastro normalmente
+    try:
+        tem_emergencia = input("Tem alguma emergência? (S/N)").upper()
+        if tem_emergencia == "S":
+            contagem_regressiva()
+        else:
+            main()  # Executa o menu de cadastro normalmente
+    except KeyboardInterrupt:
+        print("\nPrograma interrompido pelo usuário.")
